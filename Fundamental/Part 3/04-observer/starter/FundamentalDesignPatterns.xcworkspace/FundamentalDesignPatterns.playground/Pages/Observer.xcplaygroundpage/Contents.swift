2cce370928eedc1e5b2071630569ed1941e6b815
sanghunlee <simoniful0@gmail.com>
2022-07-24 12:15:29 +0900

    Add: Fundamental (7) - Observer 예제 추가
    커스텀 Observable

--- Fundamental/Part 3/04-observer/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Observer.xcplaygroundpage/Contents.swift
+++ Fundamental/Part 3/04-observer/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Observer.xcplaygroundpage/Contents.swift
@@ -12,6 +12,7 @@
  ## Code Example
  */
 
+// MARK: - Example. 1
 import Combine
 
 public class User {
@@ -33,5 +34,67 @@ user.name = "Jin"
 
 subscriber = nil
 user.name = "Lee"
+
+
+// MARK: - Example. 2
+protocol Observer {
+    var id: String { get set }
+    func update(message: String)
+}
+
+protocol Publisher {
+    var observers: [Observer] { get set }
+    func subscribe(observer: Observer)
+    func unSubscribe(observer: Observer)
+    func notify(message: String)
+}
+
+class AppleStore: Publisher {
+    var observers: [Observer]
+    
+    init(observers: [Observer]) {
+        self.observers = observers
+    }
+    
+    func subscribe(observer: Observer) {
+        self.observers.append(observer)
+    }
+    
+    func unSubscribe(observer: Observer) {
+        if let index = self.observers.firstIndex(where: { $0.id == observer.id }) {
+            self.observers.remove(at: index)
+        }
+    }
+    
+    func notify(message: String) {
+        for observer in observers {
+            observer.update(message: message)
+        }
+    }
+}
+
+class Customer: Observer {
+    var id: String
+    init(id: String) {
+        self.id = id
+    }
+    func update(message: String) {
+        print("\(id)님 \(message)수신\n")
+    }
+}
+
+let appleStore = AppleStore(observers: [])
+let pingu = Customer(id: "Pingu")
+let pinga = Customer(id: "Pinga")
+let roby = Customer(id: "Roby")
+let ick = Customer(id: "Ick")
+
+appleStore.subscribe(observer: pingu)
+appleStore.subscribe(observer: roby)
+
+appleStore.notify(message: "iPhone 재고 입고!")
+
+appleStore.unSubscribe(observer: roby)
+appleStore.notify(message: "iPad 재고 입고!")
 
 
