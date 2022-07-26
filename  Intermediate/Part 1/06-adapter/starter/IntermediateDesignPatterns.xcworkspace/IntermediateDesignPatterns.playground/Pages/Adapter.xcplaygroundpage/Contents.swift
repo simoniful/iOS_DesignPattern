16a2cbd301371297d13205fbc3d3bcce8863b543
sanghunlee <simoniful0@gmail.com>
2022-07-26 18:31:58 +0900

    Add: Intermediate (3) - Adapter
    이론 부분 정리

---  Intermediate/Part 1/06-adapter/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
+++  Intermediate/Part 1/06-adapter/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
@@ -17,3 +17,85 @@
  
  ## Code Example
  */
+
+
+import Foundation
+
+// Make networking calls, which return a special "token"
+public class GoogleAuthenticator {
+    public func login(email: String,
+                      password: String,
+                      completion: @escaping (GoogleUser?, Error?) -> Void) {
+        let token = "special-token-value"
+        let user = GoogleUser(
+            email: email,
+            password: password,
+            token: token
+        )
+        completion(user, nil)
+    }
+}
+            
+public struct GoogleUser {
+    public var email: String
+    public var password: String
+    public var token: String
+}
+
+public protocol AuthenticationService {
+    func login(
+        email: String,
+        password: String,
+        success: @escaping (User, Token) -> Void,
+        failure: @escaping (Error?) -> Void
+    )
+}
+
+public struct User {
+    public var email: String
+    public var password: String
+}
+
+public struct Token {
+    public let value: String
+}
+
+public class GoogleAuthenticatorAdapter: AuthenticationService {
+    private var authenticator = GoogleAuthenticator()
+    
+    public func login(
+        email: String,
+        password: String,
+        success: @escaping (User, Token) -> Void,
+        failure: @escaping (Error?) -> Void
+    ) {
+        authenticator.login(
+            email: email,
+            password: password) { googleUser, error in
+                guard let googleUser = googleUser else {
+                    failure(error)
+                    return
+                }
+                let user = User(email: email, password: password)
+                let token = Token(value: googleUser.token)
+                success(user, token)
+            }
+    }
+}
+
+let authService: AuthenticationService = GoogleAuthenticatorAdapter()
+authService.login(
+    email: "simon@apple.com",
+    password: "password",
+    success: { user, token in
+        print("Auth succeded: \(user.email), \(token.value)")
+    },
+    failure: { error in
+        if let error = error {
+            print("Auth failed wuth error: \(error.localizedDescription)")
+        } else {
+            print("Auth failed wuth no error provided")
+        }
+    }
+)
+
