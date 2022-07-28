4f6b8a3eba5b2e2df63309c5d53403ca8ae64970
sanghunlee <simoniful0@gmail.com>
2022-07-28 15:16:42 +0900

    Add: Intermediate (4) - Iterator
    이론 부분 정리

---  Intermediate/Part 1/08-iterator/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
+++  Intermediate/Part 1/08-iterator/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
@@ -13,3 +13,90 @@
  
  ## Code Example
  */
+
+import Foundation
+
+// Iterator Object
+public struct Queue<T> {
+    private var array: [T?] = []
+    private var head = 0
+    
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    
+    public var count: Int {
+        return array.count - head
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard head < array.count,
+        let element = array[head] else {
+            return nil
+        }
+        array[head] = nil
+        head += 1
+        let percentage = Double(head) / Double(array.count)
+        if array.count > 50,
+           percentage > 0.25 {
+            array.removeFirst(head)
+            head = 0
+        }
+        return element
+    }
+}
+
+// IteratorProtocol
+extension Queue: Sequence {
+    public func makeIterator() -> IndexingIterator<Array<T>> {
+        let nonEmptyValues = Array(array[head ..< array.count]) as! [T]
+        return nonEmptyValues.makeIterator()
+    }
+}
+
+// Model
+public struct Ticket {
+    enum PriorityType {
+        case low
+        case medium
+        case high
+    }
+    var description: String
+    var priority: PriorityType
+}
+
+extension Ticket {
+    var sortIndex: Int {
+        switch self.priority {
+        case .low:
+            return 0
+        case .medium:
+            return 1
+        case .high:
+            return 2
+        }
+    }
+}
+
+var queue = Queue<Ticket>()
+queue.enqueue(Ticket(description: "디자인 패턴 정리하기", priority: .medium))
+queue.enqueue(Ticket(description: "데이터 스트럭쳐 정리하기", priority: .high))
+queue.enqueue(Ticket(description: "포트폴리오 정리하기", priority: .low))
+queue.enqueue(Ticket(description: "이력서 정리하기", priority: .medium))
+
+print("List of Tickets in queue:")
+for ticket in queue {
+    print(ticket.description)
+}
+
+let sortedTickets = queue.sorted {
+    $0.sortIndex > $1.sortIndex
+}
+print("\nSorted Tickets in queue:")
+for ticket in sortedTickets {
+    print(ticket.description)
+}
