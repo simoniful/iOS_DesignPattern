49076c2a411ba0c62b6f49a704344b3f8c3dd913
sanghunlee <simoniful0@gmail.com>
2022-07-29 16:51:39 +0900

    Add: Intermediate (5) - Prototype
    이론 부분 정리

---  Intermediate/Part 2/02-prototype/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
+++  Intermediate/Part 2/02-prototype/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
@@ -13,3 +13,55 @@
  
  ## Code Example
  */
+
+import Foundation
+
+public protocol Prototype: AnyObject {
+    init(_ prototype: Self)
+}
+extension Prototype {
+    public func copy() -> Self {
+        return type(of: self).init(self)
+    }
+}
+
+public class Monster: Prototype {
+    public var health: Int
+    public var level: Int
+    public init(health: Int, level: Int) {
+        self.health = health
+        self.level = level
+    }
+    public required convenience init(_ prototype: Monster) {
+        self.init(health: prototype.health, level: prototype.level)
+    }
+}
+
+public class EyeballMonster: Monster {
+    public var redness = 0
+    public override convenience init(health: Int, level: Int) {
+        self.init(health: health, level: level, redness: 0)
+    }
+    public init(health: Int, level: Int, redness: Int) {
+        self.redness = redness
+        super.init(health: health, level: level)
+    }
+    
+    @available(*, unavailable, message: "Call copy() instead")
+    public required convenience init(_ prototype: Monster) {
+        let eyeballMonster = prototype as! EyeballMonster
+        self.init(health: eyeballMonster.health,
+                  level: eyeballMonster.level,
+                  redness: eyeballMonster.redness)
+    }
+}
+
+let monster = Monster(health: 700, level: 37)
+let monster2 = monster.copy()
+print("Watch out! That monster's level is \(monster2.level)")
+
+let eyeballMonster = EyeballMonster(health: 3000, level: 60, redness: 999)
+let eyeballMonster2 = eyeballMonster.copy()
+print("Eww! It's eyeball redness is \(eyeballMonster2.redness)!")
+
+// let eyeballMonster3 = EyeballMonster(monster)
