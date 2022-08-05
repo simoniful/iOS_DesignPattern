a0ac666230be4e05baaf791b72f905da27a50e17
sanghunlee <simoniful0@gmail.com>
2022-08-05 22:56:37 +0900

    Add: Intermediate (8) - Facade
    이론 부분 정리
    상품과 주문자에 대한 Dependency Model 정리, Hashable
    Database 관련 Container 구성
    Facade를 통한 주문 관련 로직 구성

---  Intermediate/Part 2/08-facade/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
+++  Intermediate/Part 2/08-facade/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -13,3 +13,104 @@
  
  ## Code Example
  */
+
+import Foundation
+
+// MARK: - Dependencies
+public struct Customer {
+    public let identifier: String
+    public var address: String
+    public var name: String
+}
+
+extension Customer: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+    public static func ==(lhs: Customer, rhs: Customer) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+}
+
+public struct Product {
+    public let identifier: String
+    public var name: String
+    public var cost: Double
+}
+
+extension Product: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+    public static func ==(lhs: Product, rhs: Product) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+}
+
+public class InventoryDatabase {
+    public var inventory: [Product: Int] = [:]
+    
+    public init(inventory: [Product: Int]) {
+        self.inventory = inventory
+    }
+}
+
+public class ShippingDatabase {
+    public var pendingShipments: [Customer: [Product]] = [:]
+}
+
+// MARK: - Facade
+public class OrderFacade {
+    public let inventoryDatabase: InventoryDatabase
+    public let shippingDatabase: ShippingDatabase
+    
+    init(inventoryDatabase: InventoryDatabase, shippingDatabase: ShippingDatabase) {
+        self.inventoryDatabase = inventoryDatabase
+        self.shippingDatabase = shippingDatabase
+    }
+    
+    public func placeOrder(for product: Product, by customer: Customer) {
+        print("Place order for '\(product.name)' by '\(customer.name)'")
+        
+        let count = inventoryDatabase.inventory[product, default: 0]
+        
+        guard count > 0 else {
+            print("'\(product.name)' is out of stock!")
+            return
+        }
+        
+        inventoryDatabase.inventory[product] = count - 1
+        var shipments = shippingDatabase.pendingShipments[customer, default: []]
+        shipments.append(product)
+        shippingDatabase.pendingShipments[customer] = shipments
+        
+        print("Order placed for '\(product.name)' by '\(customer.name)'")
+    }
+}
+
+// MARK: - Example
+let oreoOriginal = Product(identifier: "product-001", name: "Oreo Original Flavor", cost: 1.0)
+let oreoMint = Product(identifier: "product-002", name: "Oreo Mint Flavor", cost: 1.3)
+let oreoRedValvet = Product(identifier: "product-003", name: "Oreo RedValvet Flavor", cost: 1.5)
+
+let inventoryDatabase = InventoryDatabase(
+    inventory: [
+        oreoOriginal: 100,
+        oreoMint: 50,
+        oreoRedValvet: 1
+    ]
+)
+
+let orderFacade = OrderFacade(
+    inventoryDatabase: inventoryDatabase,
+    shippingDatabase: ShippingDatabase()
+)
+
+let simon = Customer(identifier: "customer-001", address: "505, Sinlimdong-gil 20, Seoul, ROK", name: "Simon")
+let jin = Customer(identifier: "customer-002", address: "515, dang-san-ro 3 gil, Seoul, ROK", name: "Jin")
+
+orderFacade.placeOrder(for: oreoRedValvet, by: simon)
+
+
