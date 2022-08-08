1e170685df68da8152f8e16a2c004472d733b383
sanghunlee <simoniful0@gmail.com>
2022-08-08 16:14:58 +0900

    Add: Advanced (1) - Flyweight
    이론 부분 정리 추가
    Bullet 관련 예시 추가
    Unshared Concrete Flyweight / Concrete Flyweight 관련 개념 포함
    Intrinsic State - 가변적인 State / Extrinsic state - 불변적인 State
    메모리 관련 절약에 주안

--- Advanced/Part 1/02-flyweight/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
+++ Advanced/Part 1/02-flyweight/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -15,6 +15,8 @@
 
 import UIKit
 
+// MARK: - Example 1
+
 let red = UIColor.red
 let red2 = UIColor.red
 
@@ -24,7 +26,6 @@ let color = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
 let color2 = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
 
 print(color === color2)
-
 
 extension UIColor {
     public static var colorStore: [String: UIColor] = [:]
@@ -53,3 +54,103 @@ let flyColor = UIColor.rgba(1, 0, 0, 1)
 let flyColor2 = UIColor.rgba(1, 0, 0, 1)
 
 print(flyColor === flyColor2)
+
+// MARK: - Example 2
+
+// Flyweight
+class BulletFlyweight {
+    // Operation
+    // Extrinsic state
+    let color: String
+    let size: CGFloat
+    
+    init(color: String, size: CGFloat) {
+        self.color = color
+        self.size = size
+    }
+}
+
+// Unshared Concrete Flyweight
+class Bullet {
+    // Intrinsic State
+    var x: CGFloat
+    var y: CGFloat
+    var velocity: CGFloat
+    
+    // Concrete Flyweight
+    private var extrinsicState: BulletFlyweight
+    
+    init(x: CGFloat, y: CGFloat, velocity: CGFloat, extrinsicState: BulletFlyweight) {
+        self.x = x
+        self.y = y
+        self.velocity = velocity
+        self.extrinsicState = extrinsicState
+    }
+    
+    func getState() {
+        print("Flyweight : \(x),\(y),\(velocity) 값을 갖는 \(extrinsicState.color), \(extrinsicState.size) 총알")
+    }
+}
+
+// Flyweight Factory
+class BulletFlyweightFactory {
+    static private var bulletFlyweightList: [BulletFlyweight] = []
+    
+    // GetFlyweight(repeating state)
+    static func getBulletFlyweight(color: String, size: CGFloat) -> BulletFlyweight {
+        if let flyweightIndex = self.bulletFlyweightList.firstIndex(where: { (bullet) -> Bool in
+            return bullet.color == color && bullet.size == size
+        }) {
+            return self.bulletFlyweightList[flyweightIndex]
+        } else {
+            self.bulletFlyweightList.append(BulletFlyweight(color: color, size: size))
+            print("\(color),\(size) Flyweight 객체 생성")
+            return self.bulletFlyweightList.last ?? BulletFlyweight(color: color, size: size)
+        }
+    }
+    
+    static var flyweightCount: Int {
+        return self.bulletFlyweightList.count
+    }
+    
+    private init() {}
+}
+
+var bullets: [Bullet] = []
+
+for i in 0..<5 {
+    let i = CGFloat(i)
+    bullets.append(
+        Bullet(
+            x: i,
+            y: i,
+            velocity: i * 5,
+            extrinsicState: BulletFlyweightFactory.getBulletFlyweight(
+                color: "Gold",
+                size: 5.56
+            )
+        )
+    )
+}
+
+for i in 0..<7 {
+    let i = CGFloat(i)
+    bullets.append(
+        Bullet(
+            x: i,
+            y: i,
+            velocity: i * 7,
+            extrinsicState: BulletFlyweightFactory.getBulletFlyweight(
+                color: "Silver",
+                size: 7.62
+            )
+        )
+    )
+}
+
+print("Flyweight 객체 수: \(BulletFlyweightFactory.flyweightCount)")
+
+
+
+
+
