fa5732571d566836c69a9bb71d59ef00f510d809
sanghunlee <simoniful0@gmail.com>
2022-08-09 22:56:00 +0900

    Add: Advanced (3) - Composite
    이론 부분 정리
    파일 구조를 활용한 패턴 예제 구성
    계층화된 트리 형태의 객체 구현

--- Advanced/Part 1/07-composite/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
+++ Advanced/Part 1/07-composite/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
@@ -13,3 +13,82 @@
  
  ## Code Example
  */
+import Foundation
+
+public protocol File {
+    var name: String { get set }
+    func open()
+}
+
+struct eBook: File {
+    var name: String
+    var author: String
+    
+    func open() {
+        print("\(author)의 \(name) iBooks에서 열림")
+    }
+}
+
+struct Music: File {
+    var name: String
+    var artist: String
+    
+    func open() {
+        print("\(artist)의 \(name) AppleMusic에서 재생")
+    }
+}
+
+struct Folder: File {
+    var name: String
+    var files: [File] = []
+    
+    mutating func addFile(_ file: File) {
+        self.files.append(file)
+    }
+    
+    func open() {
+        print("\(name) 폴더 내부 파일들")
+        for file in files {
+            print("-\(file.name)")
+        }
+    }
+}
+
+let painKiller = Music(name: "Pain Killer", artist: "HarryStyles")
+let rebelRebel = Music(name: "Rebel Rebel", artist: "David Bowie")
+let thatsLife = Music(name: "That's Life", artist: "Frank Sinatra")
+
+let object = eBook(name: "Object", author: "조영호")
+let cleanCode = eBook(name: "Clean Code", author: "Robert. C Martin")
+let cleanArchitecture = eBook(name: "Clean Architecture", author: "Robert. C Martin")
+
+var documents = Folder(name: "Documents")
+var eBookFolder = Folder(name: "My Book")
+var musicFolder = Folder(name: "My Music")
+
+documents.addFile(musicFolder)
+documents.addFile(eBookFolder)
+
+musicFolder.addFile(painKiller)
+musicFolder.addFile(rebelRebel)
+musicFolder.addFile(thatsLife)
+
+eBookFolder.addFile(object)
+eBookFolder.addFile(cleanCode)
+eBookFolder.addFile(cleanArchitecture)
+
+painKiller.open()
+object.open()
+print("")
+
+documents.open()
+print("")
+
+musicFolder.open()
+print("")
+
+
+
+
+
+
